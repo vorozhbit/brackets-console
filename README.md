56516b0a722b2576465503469c89f7c12740eac0
Andrey Vorozhbit <andrey.vorozhbit@gmail.com>
2018-01-19 13:42:54 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Brackets Checker Console Application
+
+Checks brackets in a given file.
+
+Installation
+---------
+
+```
+composer install
+```
+
+Usage
+---------
+
+```
+php bin/console app:check-brackets <testfile>
+```
---
+++ src/CheckBracketsCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Brackets\Checker;
+
+class CheckBracketsCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('app:check-brackets')
+            ->setDescription('Checks brackets in a given file.')
+            ->addArgument('file', InputArgument::REQUIRED, 'Provide path to file')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $checker = new Checker();
+
+        $file = $input->getArgument('file');
+
+        if (!file_exists($file)) {
+            $output->writeln('File does not exist!');
+            return false;
+        }
+
+        $str = file_get_contents($file);
+
+        try{
+            if($checker->check($str)){
+                $output->writeln('Brackets are ok!');
+            } else {
+                $output->writeln('Incorrect brackets!');
+            }
+
+        } catch (\InvalidArgumentException $e){
+            $output->writeln($e->getMessage());
+        }
+    }
+}
